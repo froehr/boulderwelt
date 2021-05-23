f447e8603b41b6d548f738233407188824752025
Fabian Röhr <fabian.roehr@netlight.com>
2021-05-23 17:44:14 +0200

    Add endpoint which returns all values of today and same weekday of last week for comparison in chart

--- backend/api.py
+++ backend/api.py
@@ -4,28 +4,51 @@ from dto import UtilizationDTO, BoulderworldDTO
 from db import Session
 from models import Boulderworld, Utilization
 
+from sqlalchemy import Date, cast
+from datetime import date, timedelta
+
 api = Blueprint('api', __name__)
 
 
-@api.route('/api/boulderworlds/<short_name>/utilizations', methods=['GET'])
-def get_boulderworld_utilization(short_name):
+@api.route('/api/boulderworlds/<short_name>/utilizations/today', methods=['GET'])
+def get_boulderworld_utilization_of_today(short_name):
     assert short_name == request.view_args['short_name']
     boulderworld = Session.query(Boulderworld) \
         .filter_by(short_name=short_name).first()
 
-    utilizations = Session.query(Utilization) \
-        .filter_by(boulderworld_id=boulderworld.id).order_by(
-        Utilization.id.asc())
-
-    utilization_dtos = []
-
-    for utilization in utilizations:
-        utilization_dtos.append(
-            UtilizationDTO(utilization.date_time, utilization.utilization,
-                           utilization.people_waiting))
-
+    utilization_dtos = get_utilization_x_days_ago(boulderworld, 0)
     boulderworld_dto = BoulderworldDTO(name=boulderworld.name,
                                        is_open=boulderworld.is_open,
                                        utilizations=utilization_dtos)
 
     return jsonify(boulderworld_dto)
+
+
+@api.route('/api/boulderworlds/<short_name>/utilizations/lastweek', methods=['GET'])
+def get_boulderworld_utilization_of_7_days_ago(short_name):
+    assert short_name == request.view_args['short_name']
+    boulderworld = Session.query(Boulderworld) \
+        .filter_by(short_name=short_name).first()
+
+    utilization_dtos = get_utilization_x_days_ago(boulderworld, 7)
+    boulderworld_dto = BoulderworldDTO(name=boulderworld.name,
+                                       is_open=boulderworld.is_open,
+                                       utilizations=utilization_dtos)
+
+    return jsonify(boulderworld_dto)
+
+
+def get_utilization_x_days_ago(boulderworld, days_ago):
+    utilizations = Session.query(Utilization) \
+        .filter_by(boulderworld_id=boulderworld.id) \
+        .order_by(Utilization.id.asc()) \
+        .filter(cast(Utilization.date_time, Date) == date.today() - timedelta(days=days_ago))
+
+    utilization_dtos = []
+
+    for utilization in utilizations:
+        utilization_dtos.append(
+                UtilizationDTO(utilization.date_time, utilization.utilization,
+                               utilization.people_waiting))
+
+    return utilization_dtos
